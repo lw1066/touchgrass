bb00348010b6a67afd450c26e031e5e36e761beb
lw1066 <lewis_webster@hotmail.com>
2024-03-28 16:05:04 +0000

    added singin/up using firebase auth

--- app/signup.jsx
+++ app/signup.jsx
@@ -1,117 +1,110 @@
-import {React ,useState} from 'react'
-import { View, Text, TextInput , Pressable , StyleSheet , Image , Alert } from 'react-native'
-import {Link , useRouter} from "expo-router"
-import  styles  from "../styling/styles";
+import { React, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  Image,
+  Alert,
+} from "react-native";
+import { Link, useRouter } from "expo-router";
+import styles from "../styling/styles";
+import { FIREBASE_APP, FIREBASE_AUTH } from "../firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-const SignUpScreen =() => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [postCode, setPostCode] = useState("");
-    const router = useRouter();
+const SignUpScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [postCode, setPostCode] = useState("");
+  const router = useRouter();
 
-    const handleAccountCreation = () => {
-        /**
-         * 
-         * check database if the username is not the same 
-         * use jwt to create username and hash password
-         * then use jwt to get the auth token back
-         * set isloggedin to auth token 
-         * 
-         */
-        setIsLoggedIn(true)
-        
+  const registerUser = async () => {
+    if (!username || !password || !email || !postCode) {
+      Alert.alert(
+        "Registration failed",
+        "Please enter a valid username and password",
+        [{ text: "OK", onPress: () => console.log("OK Pressed") }]
+      );
+      return;
     }
-
-    const registerUser = () => {
-        if (!username || !password || email || postCode) {
-            Alert.alert('Registration failed', 'Please enter a valid username and password', [
-                {text: 'OK', onPress: () => console.log('OK Pressed')},
-              ]);
-            return 
-         }
-         //this will be refractored
-         const simulateLogin = () => {
-            return new Promise((resolve,reject) => {
-                setTimeout(() => {
-                    resolve("login successful")          
-                  }, 3000)})
-         }
-         
-        simulateLogin()
-        .then((msg) => {
-            console.log(msg);
-            router.push("/map")
-        })
-        .catch((err) => {
-            console.log("log in failed",err);
-        })
-        //this will be refractored
-        
-      }
-  
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        FIREBASE_AUTH,
+        email,
+        password
+      );
+      const user = userCredentials.user;
+      console.log(user);
+      router.push("/map");
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error.message);
+      // Handle error
+      Alert.alert("Registration failed", errorMessage);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Image 
-            style={styles.logo}
-            source={
-               require("../assets/icon.png")
-            }/>
-    <Text style={styles.title}>Sign Up</Text>
-    <View style={styles.inputView}>
-    <TextInput  
-     style={styles.inputText}
-        value={email}
-        placeholder={"Enter Email"}
-        placeholderTextColor="#000" 
-        onChangeText={(text) => setEmail(text)}
-        autoCapitalize={"none"}
-      />
-    </View>
-    <View style={styles.inputView}>
-    <TextInput  
-     style={styles.inputText}
-        value={username}
-        placeholder={"Enter Username"}
-        placeholderTextColor="#000" 
-        onChangeText={(text) => setUsername(text)}
-        autoCapitalize={"none"}
-      />
-    </View>
-    
-    <View style={styles.inputView}>
-    <TextInput
-     style={styles.inputText}   
-        value={password}
-        placeholder={"Enter Password"}
-        placeholderTextColor="#000" 
-        secureTextEntry
-        onChangeText={(text) => setPassword(text)}
-      />
-       </View>
-       <View style={styles.inputView}>
-    <TextInput  
-     style={styles.inputText}
-        value={postCode}
-        placeholder={"Enter Post code"}
-        placeholderTextColor="#000" 
-        onChangeText={(text) => setPostCode(text)}
-        autoCapitalize={"none"}
-        maxLength={8}
-      />
-    </View>
-      
-       <Pressable style={({ pressed }) => [{ backgroundColor: pressed ? 'yellow' : 'lightgreen' }, styles.button ]} onPress={registerUser}>
-       <Text style={styles.text}>Sign up</Text>
-          </Pressable>
+      <Image style={styles.logo} source={require("../assets/icon.png")} />
+      <Text style={styles.title}>Sign Up</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          value={email}
+          placeholder={"Enter Email"}
+          placeholderTextColor="#000"
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          value={username}
+          placeholder={"Enter Username"}
+          placeholderTextColor="#000"
+          onChangeText={(text) => setUsername(text)}
+          autoCapitalize={"none"}
+        />
+      </View>
 
-          <Pressable style={({ pressed }) => [{ backgroundColor: pressed ? 'yellow' : 'lightgreen' }, styles.button ]} onPress={() => router.back()}>
-           <Text style={styles.text}>Back</Text> 
-          </Pressable>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          value={password}
+          placeholder={"Enter Password"}
+          placeholderTextColor="#000"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          value={postCode}
+          placeholder={"Enter Post code"}
+          placeholderTextColor="#000"
+          onChangeText={(text) => setPostCode(text)}
+          autoCapitalize={"none"}
+          maxLength={8}
+        />
+      </View>
+
+      <Pressable
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "yellow" : "lightgreen" },
+          styles.button,
+        ]}
+        onPress={registerUser}
+      >
+        <Text style={styles.text}>Sign up</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-
-export default SignUpScreen
+export default SignUpScreen;
