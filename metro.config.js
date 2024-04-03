19d7331702c2e24c25c21cf3f0b5c07fff317e36
justanotherchili <chiyan.li@hotmail.com>
2024-04-03 10:10:50 +0100

    Added an animated 3Dobject with onTouch functions

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,28 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+// const { getDefaultConfig } = require("@expo/metro-config");
+
+// const defaultConfig = getDefaultConfig(__dirname);
+// defaultConfig.resolver.sourceExts.push("cjs");
+
+// module.exports = defaultConfig;
+const { getDefaultConfig } = require('expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push("cjs");
+
+defaultConfig.resolver.assetExts = [
+  ...defaultConfig.resolver.assetExts,
+  'riv',
+  'obj',
+  'mtl',
+  'mp3',
+  'JPG',
+  'vrx',
+  'png',
+  'hdr',
+  'gltf',
+  'glb',
+  'bin',
+  'arobject',
+  'gif',
+];
 
 module.exports = defaultConfig;
